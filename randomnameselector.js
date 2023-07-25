d9a4767b8ca2f5292e0e63e275a747f2e38783d1
Pratik <pdfulkar@yahoo.com>
2023-07-25 15:34:09 +0530

    Improved the code of the random name selector

--- randomnameselector.js
+++ randomnameselector.js
@@ -1,7 +1,5 @@
-var name = ["rahul","pratik","jayesh","kartik","pratham","rohit","niru"];
-function randName()
-{
- var n = Math.random();
-    return (name [(n *(name.length))]);
-}
-console.log("The person who is going to pay is " + randName() +" ! ");
+var nameOf = ["rahul","pratik","jayesh","kartik","pratham","rohit","niru"];
+var n = Math.random();
+var num = Math.trunc(n *nameOf.length);
+var payee = nameOf [num];
+console.log("The person who is going to pay is " + payee +" ! ");
